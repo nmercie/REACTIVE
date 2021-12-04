0b98e7ed188db5a27aed04375edbcd9e05d5e859
dedane <evanskeema@gmail.com>
2021-12-04 21:31:47 +0000

    list-rendering

--- reactive/src/components/Expenses.js
+++ reactive/src/components/Expenses.js
@@ -16,26 +16,13 @@ function Expenses(props) {
         
         <div className="expenses">
         <ExpenseFilter selected={setDate} onDateChange={selectedDateHandler}/>   
-            <ExpenseItems 
-                title={props.items[0].title}
-                amount={props.items[0].amount}
-                date={props.items[0].date}   />
-            <ExpenseItems 
-                title={props.items[1].title}
-                amount={props.items[1].amount}
-                date={props.items[1].date}   />
-            <ExpenseItems 
-                title={props.items[2].title}
-                amount={props.items[2].amount}
-                date={props.items[2].date}   />
-            <ExpenseItems 
-                title={props.items[3].title}
-                amount={props.items[3].amount}
-                date={props.items[3].date}   />
-            <ExpenseItems 
-                title={props.items[4].title}
-                amount={props.items[4].amount}
-                date={props.items[4].date}   />
+            {props.items.map(
+                expense => 
+                <ExpenseItems
+                    title={expense.title}
+                    expense={expense.amount}
+                    date={expense.date}   
+                 /> )}
         </div>
     </div>
     )
