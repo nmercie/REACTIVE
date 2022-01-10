f52c9b77517846060d68b139959bd5f2301efb04
dedane <evanskeema@gmail.com>
2022-01-10 03:04:51 +0000

    confitional-statements

--- reactive/src/App.js
+++ reactive/src/App.js
@@ -4,39 +4,41 @@ import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses';
 import Chart from './components/Chart/ChartBar';
 
+const DUMMY_DATA = [
+  {
+    id: 'e1',
+    title: 'Working out',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { 
+   id: 'e2',
+   title: 'Meditation', 
+   amount: 79.49, 
+   date: new Date(2021, 2, 12) 
+  },
+  {
+    id: 'e3',
+    title: 'Reading Books',
+    amount: 29.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 45,
+    date: new Date(2021, 5, 12),
+  },
+  {
+    id: 'e5',
+    title: 'Eating Healthy',
+    amount: 94.12,
+    date: new Date(2021, 12, 11),
+  },
+];
+
 function App() {
-  const DUMMY_DATA = [
-    {
-      id: 'e1',
-      title: 'Working out',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { 
-     id: 'e2',
-     title: 'Meditation', 
-     amount: 79.49, 
-     date: new Date(2021, 2, 12) 
-    },
-    {
-      id: 'e3',
-      title: 'Reading Books',
-      amount: 29.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 45,
-      date: new Date(2021, 5, 12),
-    },
-    {
-      id: 'e5',
-      title: 'Eating Healthy',
-      amount: 94.12,
-      date: new Date(2021, 12, 11),
-    },
-  ];
+  
 
   const [enteredExpense, setEnteredExpense] = useState(DUMMY_DATA)
 
