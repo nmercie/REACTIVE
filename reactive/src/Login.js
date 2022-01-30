4c71472b2765ca8db3208c4d007d543afd5265b9
dedane <evanskeema@gmail.com>
2022-01-30 14:53:40 +0300

    login-page

--- reactive/src/Login.js
+++ reactive/src/Login.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 function Login() {
-  return <div>
-      
-  </div>;
+  return (
+      <Box>
+          <form>
+              <TextField 
+                id="username_input"
+                label="username"
+                />
+                <TextField
+                    id="outlined-password-input"
+                    label="password"
+                    type="password"
+                    autoComplete="current-password"
+                />
+                
+          </form>
+      </Box>
+  )
+
+  
 }
 
 export default Login;
